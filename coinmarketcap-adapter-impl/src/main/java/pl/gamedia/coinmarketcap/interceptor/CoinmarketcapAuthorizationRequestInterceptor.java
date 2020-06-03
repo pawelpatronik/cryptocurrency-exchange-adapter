59247a6bd572a83b01aebda9441f4a26e235582b
Pawel Patronik <pawel.patronik@gmail.com>
2020-06-03 13:08:20 +0200

    Cleanup, refactor, improved logging

--- coinmarketcap-adapter-impl/src/main/java/pl/gamedia/coinmarketcap/interceptor/CoinmarketcapAuthorizationRequestInterceptor.java
+++ coinmarketcap-adapter-impl/src/main/java/pl/gamedia/coinmarketcap/interceptor/CoinmarketcapAuthorizationRequestInterceptor.java
@@ -1,15 +1,23 @@
 package pl.gamedia.coinmarketcap.interceptor;
 
+import io.vavr.control.Try;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+import pl.gamedia.exception.DataRetrievalException;
 
 import java.io.IOException;
 
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.Predicates.instanceOf;
+
 @Component
+@Slf4j
 public class CoinmarketcapAuthorizationRequestInterceptor implements ClientHttpRequestInterceptor {
 
 	private final String apiKeyHeader;
@@ -25,6 +33,10 @@ public class CoinmarketcapAuthorizationRequestInterceptor implements ClientHttpR
 	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
 		request.getHeaders().set(apiKeyHeader, apiKeyValue);
-		return execution.execute(request, body);
+		return Try.of(() -> execution.execute(request, body))
+				.onSuccess(response -> log.debug("Successful client call execution: " + response.toString()))
+				.onFailure(response -> log.debug("Failure during client call execution: " + response.toString()))
+				.mapFailure(Case($(instanceOf(InterruptedException.class)), ex -> new DataRetrievalException("Failure during client call execution", ex)))
+				.get();
 	}
 }
